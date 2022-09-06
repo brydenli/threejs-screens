531dbc6adc843051bd206e9993d3c5ab74a9e3f0
brydenli <73043856+brydenli@users.noreply.github.com>
2022-09-05 21:30:20 -0600

    Update camera aspect ratio and renderer size when window is resized

--- src/main.js
+++ src/main.js
@@ -141,3 +141,11 @@ const animate = () => {
 };
 
 animate();
+
+const onWindowResize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
