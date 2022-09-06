16d650efa39e7aa216b6a2b1967df118a3e72e4d
brydenli <73043856+brydenli@users.noreply.github.com>
2022-09-05 20:58:05 -0600

    Use 3D models for screens, replace floor with blurred reflector

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Reflector } from 'three/examples/jsm/objects/Reflector';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight);
@@ -10,62 +11,127 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.set(0, 60, 100);
+camera.position.set(0, 50, 120);
 
-const floorTexture = new THREE.TextureLoader().load('floor.png');
-const floorGeometry = new THREE.PlaneGeometry(300, 300);
-const floorMaterial = new THREE.MeshPhongMaterial({
-  color: '#020202', shininess: 30, map: floorTexture,
-});
-const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+const floorGeometry = new THREE.PlaneGeometry(1000, 200);
+const floor = new Reflector(floorGeometry);
 floor.rotateX(-Math.PI * 0.5);
-floor.position.z = 30;
+floor.position.set(0, -10, 30);
 scene.add(floor);
+
+const blurFloorMaterial = new THREE.MeshStandardMaterial({
+  color: "#000000",
+  transparent: true,
+  opacity: 0.9,
+})
+const blurFloor = new THREE.Mesh(floorGeometry, blurFloorMaterial);
+blurFloor.rotateX(-Math.PI * 0.5);
+blurFloor.position.set(0, -9, 30);
+scene.add(blurFloor);
+
+const ambientLight = new THREE.AmbientLight('#FFFFFF', 1.5);
+scene.add(ambientLight);
 
 const gltfLoader = new GLTFLoader();
 gltfLoader.load('./text.glb', (glb) => {
   const text = glb.scene;
-  text.scale.set(10, 10, 10);
-  text.position.set(-50, 5, 15);
-  text.rotateX(0.3);
+  text.scale.set(5.5, 5.5, 5.5);
+  text.position.set(-20, -5, 20);
+  text.rotateX(0.5);
   scene.add(text);
 });
 
-const screenGeometry = new THREE.PlaneGeometry(40, 22.5);
+// desktop 1
+gltfLoader.load('./screen_desktop.glb', (glb) => {
+  const desktop = glb.scene;
+  desktop.scale.set(15, 15, 15);
+  desktop.position.set(-53, 10, 11);
+  desktop.rotateX(Math.PI / 2);
+  desktop.rotateY(Math.PI);
+  desktop.rotateZ(0.4);
 
-const createScreenRow = (height, numberOfScreens) => {
-  const offsetX = ((numberOfScreens - 1) * 43.5) / 2;
-  for (let i = 0; i < numberOfScreens; i += 1) {
-    const x = i * 45 - offsetX;
-    const y = height + 22.5;
-    const randColor = (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+  const pointLight = new THREE.PointLight('#1F51FF', 1.2);
+  pointLight.position.set(-53, 10, 0);
+  scene.add(desktop, pointLight);
+});
 
-    const screenMaterial = new THREE.MeshBasicMaterial({ color: `#${randColor}` });
-    const screen = new THREE.Mesh(screenGeometry, screenMaterial);
-    screen.position.set(x, y);
+// desktop 2
+gltfLoader.load('./screen_desktop2.glb', (glb) => {
+  const desktop = glb.scene;
+  desktop.scale.set(15, 15, 15);
+  desktop.position.set(0, 10, 0);
+  desktop.rotateX(Math.PI / 2);
+  desktop.rotateY(Math.PI);
 
-    const pointLight = new THREE.PointLight(`#${randColor}`);
-    pointLight.position.set(x, y);
+  const pointLight = new THREE.PointLight('#1F51FF', 1.2);
+  pointLight.position.set(0, 10, 0);
+  scene.add(desktop, pointLight);
+});
 
-    scene.add(screen, pointLight);
-  }
-};
+// desktop 3
+gltfLoader.load('./screen_desktop3.glb', (glb) => {
+  const desktop = glb.scene;
+  desktop.scale.set(15, 15, 15);
+  desktop.position.set(53, 10, 11);
+  desktop.rotateX(Math.PI / 2);
+  desktop.rotateY(Math.PI);
+  desktop.rotateZ(-0.4);
 
-const createScreens = (numberOfRows, screensPerRow) => {
-  for (let i = 0; i < numberOfRows; i += 1) {
-    createScreenRow(i * 26, screensPerRow);
-  }
-};
+  const pointLight = new THREE.PointLight('#1F51FF', 1.2);
+  pointLight.position.set(53, 10, 0);
+  scene.add(desktop, pointLight);
+});
 
-createScreens(2, 3);
+// desktop 4
+gltfLoader.load('./screen_desktop4.glb', (glb) => {
+  const desktop = glb.scene;
+  desktop.scale.set(15, 15, 15);
+  desktop.position.set(-53, 41, 11);
+  desktop.rotateX(Math.PI / 2);
+  desktop.rotateY(Math.PI);
+  desktop.rotateZ(0.4);
+
+  const pointLight = new THREE.PointLight('#1F51FF', 1.2);
+  pointLight.position.set(-53, 41, 0);
+  scene.add(desktop, pointLight);
+});
+
+// desktop 5
+gltfLoader.load('./screen_desktop5.glb', (glb) => {
+  const desktop = glb.scene;
+  desktop.scale.set(15, 15, 15);
+  desktop.position.set(0, 41, 0);
+  desktop.rotateX(Math.PI / 2);
+  desktop.rotateY(Math.PI);
+
+  const pointLight = new THREE.PointLight('#1F51FF', 1.2);
+  pointLight.position.set(0, 41, 0);
+  scene.add(desktop, pointLight);
+});
+
+// desktop 6
+gltfLoader.load('./screen_desktop2.glb', (glb) => {
+  const desktop = glb.scene;
+  desktop.scale.set(15, 15, 15);
+  desktop.position.set(53, 41, 11);
+  desktop.rotateX(Math.PI / 2);
+  desktop.rotateY(Math.PI);
+  desktop.rotateZ(-0.4);
+
+  const pointLight = new THREE.PointLight('#1F51FF', 1.2);
+  pointLight.position.set(53, 41, 0);
+  scene.add(desktop, pointLight);
+});
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.maxAzimuthAngle = 0.4;
-controls.minAzimuthAngle = -0.4;
+controls.maxAzimuthAngle = 0.6;
+controls.minAzimuthAngle = -0.6;
 controls.maxPolarAngle = 1.5;
-controls.minPolarAngle = 1;
-controls.maxDistance = 150;
-controls.minDistance = 100;
+controls.minPolarAngle = .8;
+controls.maxDistance = 130;
+controls.minDistance = 50;
+controls.enableDamping = true;
+controls.screenSpacePanning = false;
 
 const animate = () => {
   requestAnimationFrame(animate);
